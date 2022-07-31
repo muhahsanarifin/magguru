0c69681d52a46aafc809bf86fd7391495726bae8
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-07-31 20:40:05 +0700

    Updated Files

--- script.js
+++ script.js
@@ -31,10 +31,13 @@ fetch("./assets/courses.json")
 	for(let courses of datas) {
 		output += `
 			<article class="article-content">
-	 			<img src="${courses.img}" alt="Image">
-				<span>
+	 			<img src="${courses.img}" alt="Image" class="logo-courses">
+				<span class="identity-courses">
 					<h3>${courses.nama}</h3>
-					<a href="${courses.url}" target="_blank"><p>Kunjungi Website</p></a>
+					<div class="contact-course">
+						<a href="${courses.url}" target="_blank"><img src="./assets/icon/website-icon-course.svg" alt="Website Icon" class="website-icon-courses"></a>
+						<a href="${courses.url_instagram}" target="_blank"><img src="./assets/icon/instagram-icon-course.svg" alt="Instagram Icon" class="instagram-icon-courses"></a>
+					</div>
 				</span>
 			</article>	
 		`;
