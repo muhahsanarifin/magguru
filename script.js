bbd3930b27235e281520f2908b22eaf8f54fdc52
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-09-06 22:57:02 +0700

    Updated files

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // || Info
-
 document.getElementById("showInfo").addEventListener("click", function () {
   toogle(document.getElementById("infoProfiles"));
 });
@@ -10,20 +9,20 @@ function toogle(event) {
 
 // Menutup Dropdown ketika user melakukan klik diluar dari objek dropdown
 /*window.onclick = function(param){
-	
-	if(!param.target.matches('.info')){	
+  
+  if(!param.target.matches('.info')){	
 
-		let profileContent = document.getElementsByClassName("profiles");
+    let profileContent = document.getElementsByClassName("profiles");
 
-			for (let showProfileContent of profileContent) {
+      for (let showProfileContent of profileContent) {
         
         if(showProfileContent.classList.contains('info-profiles')) {
           
-			  showProfileContent.classList.remove('info-profiles');
+        showProfileContent.classList.remove('info-profiles');
 
-			}
-		}
-	}
+      }
+    }
+  }
 }*/
 
 // || Targets
@@ -32,33 +31,33 @@ const toClick = document.getElementsByTagName("a");
 let index = '';
 
 for (index = 0; index < toClick.length; index++) {
-	toClick[index].setAttribute("target","_blank");
+  toClick[index].setAttribute("target","_blank");
 }
 
 
 // || Mengambil Data dari file course.json
 fetch("./assets/courses.json")
 .then(function(respons){
-	return respons.json();
+  return respons.json();
 })
 .then(function(datas){
 
-	let output = '';
+  let output = '';
 
-	for(let courses of datas) {
-		output += `
-			<article class="article-content">
-	 			<img src="${courses.img}" alt="Image" class="logo-courses">
-				<span class="identity-courses">
-					<h3>${courses.nama}</h3>
-					<div class="contact-course">
-						<a href="${courses.url}" target="_blank"><img src="./assets/icon/website-icon-course.svg" alt="Website Icon" class="website-icon-courses"></a>
-						<a href="${courses.url_instagram}" target="_blank"><img src="./assets/icon/instagram-icon-course.svg" alt="Instagram Icon" class="instagram-icon-courses"></a>
-					</div>
-				</span>
-			</article>	
-		`;
-	}
+  for(let courses of datas) {
+    output += `
+      <article class="article-content">
+         <img src="${courses.img}" alt="Image" class="logo-courses">
+        <span class="identity-courses">
+          <h3>${courses.nama}</h3>
+          <div class="contact-course">
+            <a href="${courses.url}" target="_blank"><img src="./assets/icon/website-icon-course.svg" alt="Website Icon" class="website-icon-courses"></a>
+            <a href="${courses.url_instagram}" target="_blank"><img src="./assets/icon/instagram-icon-course.svg" alt="Instagram Icon" class="instagram-icon-courses"></a>
+          </div>
+        </span>
+      </article>	
+    `;
+  }
 
-	document.getElementById("article").innerHTML = output;
+  document.getElementById("article").innerHTML = output;
 });
